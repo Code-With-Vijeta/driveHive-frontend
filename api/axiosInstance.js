5ec321ad25560a1fa709740ee79c9cd12232ae67
Code-With-Vijeta <codevijeta@gmail.com>
2025-09-12 22:05:45 +0530

    first commit

--- api/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "https://drive-hive-backend.vercel.app/", 
-  withCredentials: true, 
-});
-
-export default axiosInstance;
