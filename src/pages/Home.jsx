321b1cbf82536fa383ccc296a0954f0023714720
Code-With-Vijeta <codevijeta@gmail.com>
2025-09-29 23:11:13 +0530

    fix: Finalizing Vercel environment variables and loading check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,29 +3,16 @@ import Hero from "../components/Hero";
 import FeaturedSection from "../components/FeaturedSection";
 import Banner from "../components/Banner";
 import Newsletter from "../components/Newsletter";
-import { useAppContext } from "../context/AppContext"; 
 
 const Home = () => {
-    const { loading, error } = useAppContext();
-
-    if (loading) {
-        return <div className="text-center p-20 text-xl">Loading homepage content...</div>; 
-    }
-
-    if (error) {
-        return <div className="text-red-600 text-center p-20 text-xl">Error: Could not load featured content. Please check server logs.</div>;
-    }
-
-    return (
-        <>
-            <Hero />
-            
-            <FeaturedSection /> 
-            
-            <Banner />
-            <Newsletter />
-        </>
-    );
+  return (
+    <>
+      <Hero />
+      <FeaturedSection />
+      <Banner />
+      <Newsletter />
+    </>
+  );
 };
 
 export default Home;
